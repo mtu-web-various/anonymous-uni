fb6466527bb9afdeba4820cc40d963e2ff3fcae1
talha <talhamucar@gmail.com>
2019-04-19 14:19:09 -0400

    components are created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,27 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { AccountPageComponent } from './account-page/account-page.component';
+import { AdsComponent } from './ads/ads.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { PostlistComponent } from './posts/postlist/postlist.component';
+import { PostcreateComponent } from './posts/postcreate/postcreate.component';
+import { TopiccreateComponent } from './topics/topiccreate/topiccreate.component';
+import { TopiclistComponent } from './topics/topiclist/topiclist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AccountPageComponent,
+    AdsComponent,
+    LoginComponent,
+    SignupComponent,
+    PostlistComponent,
+    PostcreateComponent,
+    TopiccreateComponent,
+    TopiclistComponent
   ],
   imports: [
     BrowserModule,
